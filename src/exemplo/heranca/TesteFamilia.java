98eefb2a99594d02f7b9134a5559617f86769bf9
Anderson Piotto <piottok10@gmail.com>
2023-01-16 23:07:05 -0300

    inicio de herança

--- src/exemplo/heranca/TesteFamilia.java
+++ src/exemplo/heranca/TesteFamilia.java
@@ -2,7 +2,6 @@ package exemplo.heranca;
 
 public class TesteFamilia {
 
-
     public static void main(String[] args) {
         ClasseFilha1 filha1 = new ClasseFilha1();
         filha1.getAtributo1();
